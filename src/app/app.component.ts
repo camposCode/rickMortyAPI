3a2523ebad9608b84b2b93c16215517fe85c247d
camposCode <info@josecampos.io>
2021-11-23 18:39:05 +0100

    layout de personaje hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,17 @@ import { PersonajesService } from './personajes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  arrPersonajes: any[];
 
-  constructor(private personajesService: PersonajesService){
+  constructor(private personajesService: PersonajesService) {}
 
+  ngOnInit(){
+    this.personajesService.getAll()
+    .then(response => {
+      this.arrPersonajes = response['results'];
+    })
   }
 }
